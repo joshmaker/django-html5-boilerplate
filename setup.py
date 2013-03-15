a573d5076f6c5b5a9b0aa33fcf56f4e904424461
Josh West <joshmaker@gmail.com>
2013-03-14 22:38:45 -0400

    Converted HTML5 boilerplate into a Django app

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+        name='Django HTML5 Boilerplate',
+        version='4.1.0',
+        url='https://github.com/joshmaker/django-html5-boilerplate',
+        description='Pip module for using HTML5 boilerplate with Django',
+)
